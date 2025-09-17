7cf9524f950a7896e32bfe16aff2b284ba304d18
Samuel Croteau <SCroteau94@gmail.com>
2025-09-16 22:09:53 -0400

    Reverts and simplifies GPIO mapping
    Reverts the GPIO mapping to a smaller, more specific set of pins.
    This simplifies the configuration and focuses on the pins currently used.
    
    Removes unused GPIO routes.

--- api/hardware.py
+++ api/hardware.py
@@ -277,35 +277,7 @@ def diagnose_gpio_access():
 
 
 GPIO_MAP = {
-    # 23: board.D16, # Pin 16 - GPIO 23
-    # 24: board.D18, # Pin 18 - GPIO 24    
-    # 25: board.D22 # Pin 22 - GPIO 25
-
-    1: board.D1,
-    2: board.D2,
-    3: board.D3,
-    4: board.D4,
-    5: board.D5,
-    6: board.D6,
-    7: board.D7,
-    8: board.D8,
-    9: board.D9,
-    10: board.D10,
-    11: board.D11,
-    12: board.D12,
-    13: board.D13,
-    14: board.D14,
-    15: board.D15,
-    16: board.D16,
-    17: board.D17,
-    18: board.D18,
-    19: board.D19,
-    20: board.D20,
-    21: board.D21,
-    22: board.D22,
-    23: board.D23,
-    24: board.D24,
-    25: board.D25,
-    26: board.D26,
-    27: board.D27,
+    23: board.D16, # Pin 16 - GPIO 23
+    24: board.D18, # Pin 18 - GPIO 24    
+    25: board.D22 # Pin 22 - GPIO 25
 }
